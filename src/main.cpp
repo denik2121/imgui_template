e80cea6ebe3b891b25d81f8221120623f5ce8e0c
maidamai0 <yuantonghao@gmail.com>
2021-08-23 21:45:21 +0800

    feat: window

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "assets/droid_sans.h"
 #include "assets/icon.png.h"
 #include "log.h"
+#include "window.h"
 
 int main(int argc, char** argv) {
   make_singleton<Log>();
@@ -18,7 +19,7 @@ int main(int argc, char** argv) {
   const char* glsl_version = "#version 130";
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  auto* window = glfwCreateWindow(800, 600, APP_NAME, nullptr, nullptr);
+  auto* window = glfwCreateWindow(1080, 720, APP_NAME, nullptr, nullptr);
   if (window == nullptr) {
     LOGE("Failed to create GLFW window");
     glfwTerminate();
@@ -61,7 +62,24 @@ int main(int argc, char** argv) {
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
-    ImGui::ShowDemoWindow();
+
+    // background
+    ImGuiWindowFlags background_flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
+                                        ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoScrollbar |
+                                        ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoInputs |
+                                        ImGuiWindowFlags_AlwaysAutoResize;
+    const auto size = ImGui::GetMainViewport()->WorkSize;
+    ImGui::SetNextWindowSize(size);
+    ImGui::SetNextWindowPos(ImVec2(0, 0));
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+    ImGui::Begin(APP_NAME, nullptr, background_flags);
+    ImGui::End();
+    ImGui::PopStyleVar(3);
+
+    // custom UI from here
+    make_singleton<Window>().Show();
 
     ImGui::Render();
     int display_w, display_h;
