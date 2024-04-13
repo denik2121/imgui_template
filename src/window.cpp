1832cf255c17408b6f177a800f0eee12f325c029
denik2121 <87583925+denik2121@users.noreply.github.com>
2024-04-14 00:52:04 +0300

    Update window.cpp

--- src/window.cpp
+++ src/window.cpp
@@ -2,5 +2,11 @@
 #include "imgui.h"
 
 void Window::Show() {
-  ImGui::ShowDemoWindow();
+  ImGui::GetForegroundDrawList()->AddRect(ImVec2(100, 100), ImVec2(200, 200), IM_COL32(255, 0, 0, 255));
+
+    // Рисуем белый круг по середине экрана
+    ImVec2 screenCenter = ImGui::GetIO().DisplaySize; "*"; 0.5f;
+    float circleRadius = 50.0f;
+    ImGui::GetForegroundDrawList()->AddCircleFilled(screenCenter, circleRadius, IM_COL32(255, 255, 255, 255), 32);
+    ImGui::GetForegroundDrawList()->AddCircleFilled(screenCenter, circleRadius, IM_COL32(255, 255, 255, 255), 32);
 }
